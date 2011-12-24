f0eefe10ef1c7338fc17837804132f4cfea711f3
Dan Ziemba <zman0900@gmail.com>
2011-12-23 20:47:44 -0500

    THIS IS THE ONE THAT ENABLES MTD! See all the previous commits for credits for the people who wrote this.

--- Kernel/arch/arm/mach-s5pv210/mach-victory.c
+++ Kernel/arch/arm/mach-s5pv210/mach-victory.c
@@ -158,7 +158,8 @@ static int victory_notifier_call(struct notifier_block *this,
 		else if (!strcmp((char *)_cmd, "arm9_fota"))
 			mode = REBOOT_MODE_ARM9_FOTA;
 		else if (!strcmp((char *)_cmd, "recovery"))
-			mode = REBOOT_MODE_RECOVERY;
+		   //Originally mode = REBOOT_MODE_RECOVERY;
+			mode = REBOOT_MODE_ARM11_FOTA;
 		else if (!strcmp((char *)_cmd, "bootloader"))
 			mode = REBOOT_MODE_FAST_BOOT;
 		else if (!strcmp((char *)_cmd, "download"))
@@ -3140,7 +3141,9 @@ static struct platform_device *victory_devices[] __initdata = {
 #ifdef CONFIG_FIQ_DEBUGGER
 	&s5pv210_device_fiqdbg_uart2,
 #endif
-	&s5pc110_device_onenand,
+#ifdef CONFIG_MTD_ONENAND
+    &s5p_device_onenand,
+#endif
 #ifdef CONFIG_RTC_DRV_S3C
 	&s5p_device_rtc,
 #endif
@@ -3259,7 +3262,7 @@ static void __init victory_map_io(void)
 	s3c24xx_init_uarts(victory_uartcfgs, ARRAY_SIZE(victory_uartcfgs));
 	s5p_reserve_bootmem(victory_media_devs, ARRAY_SIZE(victory_media_devs));
 #ifdef CONFIG_MTD_ONENAND
-	s5pc110_device_onenand.name = "s5pc110-onenand";
+	s5p_device_onenand.name = "s5p-onenand";
 #endif
 }
 
