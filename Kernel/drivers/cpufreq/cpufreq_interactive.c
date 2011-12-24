05bfdc04df3d8f995d67e8ff96555795c1a832c2
Dan Ziemba <zman0900@gmail.com>
2011-12-23 20:01:07 -0500

    Revert "cpufreq interactive governor save/restore IRQs around cpumask spinlocks"
    This reverts commit 3e461d60bbd6e2c6f68d8151bd91a853bf44f0fa.

--- Kernel/drivers/cpufreq/cpufreq_interactive.c
+++ Kernel/drivers/cpufreq/cpufreq_interactive.c
@@ -180,7 +180,6 @@ static void cpufreq_interactive_timer(unsigned long data)
 	u64 now_idle;
 	unsigned int new_freq;
 	unsigned int index;
-	unsigned long flags;
 
 	/*
 	 * Once pcpu->timer_run_time is updated to >= pcpu->idle_exit_time,
@@ -281,18 +280,18 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	if (new_freq < pcpu->target_freq) {
 		pcpu->target_freq = new_freq;
-		spin_lock_irqsave(&down_cpumask_lock, flags);
+		spin_lock(&down_cpumask_lock);
 		cpumask_set_cpu(data, &down_cpumask);
-		spin_unlock_irqrestore(&down_cpumask_lock, flags);
+		spin_unlock(&down_cpumask_lock);
 		queue_work(down_wq, &freq_scale_down_work);
 	} else {
 		pcpu->target_freq = new_freq;
 #if DEBUG
 		up_request_time = ktime_to_us(ktime_get());
 #endif
-		spin_lock_irqsave(&up_cpumask_lock, flags);
+		spin_lock(&up_cpumask_lock);
 		cpumask_set_cpu(data, &up_cpumask);
-		spin_unlock_irqrestore(&up_cpumask_lock, flags);
+		spin_unlock(&up_cpumask_lock);
 		wake_up_process(up_task);
 	}
 
@@ -424,7 +423,6 @@ static int cpufreq_interactive_up_task(void *data)
 {
 	unsigned int cpu;
 	cpumask_t tmp_mask;
-	unsigned long flags;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
 #if DEBUG
@@ -435,16 +433,16 @@ static int cpufreq_interactive_up_task(void *data)
 
 	while (1) {
 		set_current_state(TASK_INTERRUPTIBLE);
-		spin_lock_irqsave(&up_cpumask_lock, flags);
+		spin_lock(&up_cpumask_lock);
 
 		if (cpumask_empty(&up_cpumask)) {
-			spin_unlock_irqrestore(&up_cpumask_lock, flags);
+			spin_unlock(&up_cpumask_lock);
 			schedule();
 
 			if (kthread_should_stop())
 				break;
 
-			spin_lock_irqsave(&up_cpumask_lock, flags);
+			spin_lock(&up_cpumask_lock);
 		}
 
 		set_current_state(TASK_RUNNING);
@@ -463,7 +461,7 @@ static int cpufreq_interactive_up_task(void *data)
 
 		tmp_mask = up_cpumask;
 		cpumask_clear(&up_cpumask);
-		spin_unlock_irqrestore(&up_cpumask_lock, flags);
+		spin_unlock(&up_cpumask_lock);
 
 		for_each_cpu(cpu, &tmp_mask) {
 			pcpu = &per_cpu(cpuinfo, cpu);
@@ -490,13 +488,12 @@ static void cpufreq_interactive_freq_down(struct work_struct *work)
 {
 	unsigned int cpu;
 	cpumask_t tmp_mask;
-	unsigned long flags;
 	struct cpufreq_interactive_cpuinfo *pcpu;
 
-	spin_lock_irqsave(&down_cpumask_lock, flags);
+	spin_lock(&down_cpumask_lock);
 	tmp_mask = down_cpumask;
 	cpumask_clear(&down_cpumask);
-	spin_unlock_irqrestore(&down_cpumask_lock, flags);
+	spin_unlock(&down_cpumask_lock);
 
 	for_each_cpu(cpu, &tmp_mask) {
 		pcpu = &per_cpu(cpuinfo, cpu);
