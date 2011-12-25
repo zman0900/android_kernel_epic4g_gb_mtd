d8628eed6dd897c746630b5f80e00dc17b65cc37
Dan Ziemba <zman0900@gmail.com>
2011-12-25 00:02:37 -0500

    Switch to using mkbootfs (thanks nubecoder)

--- bundle.sh
+++ bundle.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script requires root access to set proper file permissions" 1>&2
-   exec sudo bash `basename $0` $EUID
-fi
-
-USER=$1
-
 # Clear old stuff
 rm -f `pwd`/boot.img
 rm -f `pwd`/boot.cpio.gz
@@ -22,11 +14,8 @@ find ./ -type d -exec chmod 755 '{}' \;
 chmod 750 init init.rc init.goldfish.rc
 find ./sbin -exec chmod 750 '{}' \;
 
-# Change owner to root
-find ./ -exec chown -R root:root '{}' \;
-
 # Bundle
-find ./ | cpio -o -c | gzip > ../boot.cpio.gz
+../mkbootfs `pwd` | gzip > ../boot.cpio.gz
 
 # Change back owner
 find ./ -exec chown -R $USER:$USER '{}' \;
@@ -42,14 +31,8 @@ find ./ -type d -exec chmod 755 '{}' \;
 chmod 750 init init.rc
 find ./sbin -exec chmod 750 '{}' \;
 
-# Change owner to root
-find ./ -exec chown -R root:root '{}' \;
-
 # Bundle
-find ./ | cpio -o -c | gzip > ../recovery.cpio.gz
+../mkbootfs `pwd` | gzip > ../recovery.cpio.gz
 
-# Change back owner
-find ./ -exec chown -R $USER:$USER '{}' \;
-chown $USER:$USER ../recovery.cpio.gz
 cd ..
 
