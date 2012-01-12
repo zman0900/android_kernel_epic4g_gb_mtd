32fb33f0b05256cc7318d2453f359185b491467d
Dan Ziemba <zman0900@gmail.com>
2012-01-12 03:54:28 -0500

    Increment version

--- build_kernel.sh
+++ build_kernel.sh
@@ -48,7 +48,7 @@ fi
 TARGET_LOCALE="vzw"
 
 #uncomment to add custom version string
-CUSTOMVERSION="KernelSandersCM-v2"
+CUSTOMVERSION="KernelSandersCM-v3"
 export KBUILD_BUILD_VERSION=$CUSTOMVERSION
 LOCALVERSION_STRING="-$CUSTOMVERSION"
 DEFCONFIG_STRING=cyanogenmod_epicmtd_defconfig
